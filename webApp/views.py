a998eaef0c1764c7ab59068160dee17fd1c48487
Motaz <m.gharbia@gmail.com>
2016-01-13 00:18:19 +0200

    importing additional configurations

--- webApp/views.py
+++ webApp/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
-
 def index(request):
     return HttpResponse('Hello from Python!')
     #return render(request, 'index.html')
