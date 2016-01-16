72211fa040144451527f2ad34f5907f75286709d
Motaz <m.gharbia@gmail.com>
2016-01-16 07:23:31 +0200

    Adding 1st DB example

--- webApp/views.py
+++ webApp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Package
+
 def index(request):
     #return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+	package = Package()
+	package.save()
+	
+	packages = Packages.objects.all()
+	
+    return render(request, 'index.html', {'packages': packages})
 
