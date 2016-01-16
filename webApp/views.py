9f077a9004f27fc8e067e0e3f4f7e99e249f3c83
Motaz <m.gharbia@gmail.com>
2016-01-16 23:02:15 +0200

    importing redirect

--- webApp/views.py
+++ webApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.context_processors import csrf
+from django.shortcuts import redirect
 
 from .models import Package
 
