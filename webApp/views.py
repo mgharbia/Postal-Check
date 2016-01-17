9e048c48169321f0de262f47db31f189170644b4
Motaz <m.gharbia@gmail.com>
2016-01-17 23:36:37 +0200

    re-add POST elements

--- webApp/views.py
+++ webApp/views.py
@@ -21,14 +21,14 @@ def add(request):
     return render(request, 'add.html', c)
 	
 def newitem(request):
-    #def post(self, request, *args, **kwargs):
-        #ItemName = self.request.get('Name')
-        #ItemNumber = self.request.get('TrackNumber')
+    def post(self):
+        ItemName = self.request.get('Name')
+        ItemNumber = self.request.get('TrackNumber')
     
         #order = Order(name='test', trackNumber='test num', status='NA')
         order = Order()
-        order.name = 'test'
-        order.trackNumber = '123'
+        order.name = ItemName
+        order.trackNumber = ItemNumber
         order.status = 'NA'
         order.save()
     
