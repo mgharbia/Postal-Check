fae0078452364011c49e60f850eaa86f3cd2ad0e
Motaz <m.gharbia@gmail.com>
2016-01-17 23:26:12 +0200

    DB test

--- webApp/views.py
+++ webApp/views.py
@@ -21,16 +21,16 @@ def add(request):
     return render(request, 'add.html', c)
 	
 def newitem(request):
-    #def post(self, request, *args, **kwargs):
-        #ItemName = self.request.get('Name')
-        #ItemNumber = self.request.get('TrackNumber')
+    def post(self, request, *args, **kwargs):
+        ItemName = self.request.get('Name')
+        ItemNumber = self.request.get('TrackNumber')
     
         #order = Order(name='test', trackNumber='test num', status='NA')
-        #order = Order()
-        #order.name = 'test'
-        #order.trackNumber = '123'
-        #order.status = 'NA'
-        #order.save()
+        order = Order()
+        order.name = 'test'
+        order.trackNumber = '123'
+        order.status = 'NA'
+        order.save()
     
-        #return redirect('./')
-        return HttpResponse('Saved')
+        return redirect('./')
+        #return HttpResponse('Saved')
