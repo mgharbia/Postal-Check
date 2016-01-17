74c40aa2465d0694821767a83b5b9c204b033332
Motaz <m.gharbia@gmail.com>
2016-01-17 23:14:56 +0200

    connecting POST handler in urls.py

--- Postal-Check/urls.py
+++ Postal-Check/urls.py
@@ -5,4 +5,5 @@ import webApp.views
 urlpatterns = patterns('',
     url(r'^$', webApp.views.index, name='index'),
     url(r'^add', webApp.views.add, name='add'),
+	url(r'^additem', webApp.views.additem.as_view(), name='additem'),
 )
