b9f80875b01c5cea6da12cdcf68c841f2dca4415
Motaz <m.gharbia@gmail.com>
2016-01-19 18:11:15 +0200

    clean up + testing

--- webApp/views.py
+++ webApp/views.py
@@ -9,23 +9,19 @@ import urllib3
 from .models import Order
 
 def index(request):
-    #return HttpResponse('Hello from Python!')
     url = "http://www.israelpost.co.il/itemtrace.nsf/trackandtraceJSON?openagent&_=1375340219593&lang=EN&itemcode="
-    #bad track RP325501964SG
-    #good track RB710452392CN
 		
     orders = Order.objects.all()
-    for order in orders:
-        if order.status == 'NA':
-            http = urllib3.PoolManager()
-            response = http.urlopen('GET',url + order.trackNumber, preload_content=False)
+ #   for order in orders:
+ #       if order.status == 'NA':
+ #           http = urllib3.PoolManager()
+ #           response = http.urlopen('GET',url + order.trackNumber, preload_content=False)
 
-            jasonString = response.read().decode('utf-8')
-            if jasonString.find('for distribution at the customer') != -1:
-                order.status = 'Arrived'
-                order.save()
-            #else:
-            #    return HttpResponse('NA')
+ #           jasonString = response.read().decode('utf-8')
+ #           if jasonString.find('for distribution at the customer') != -1:
+ #               order.status = 'Arrived'
+ #               order.save()
+
     return render(request, 'index.html', {'orders': orders})
 
 	
@@ -39,12 +35,11 @@ class newitem(View):
         ItemName = self.request.POST.get("Name", "")
         ItemNumber = self.request.POST.get("TrackNumber", "")
     
-        #order = Order(name='test', trackNumber='test num', status='NA')
         order = Order()
         order.name = ItemName
         order.trackNumber = ItemNumber
         order.status = 'NA'
         order.save()
     
-        #return redirect('./')
-        return HttpResponse('added')
+        return redirect('./')
+        #return HttpResponse('added')
